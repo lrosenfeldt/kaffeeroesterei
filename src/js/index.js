f282a7f702a8b24f1c01ab9b60a2c482e628ead8
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2021-02-11 13:04:55 +0100

    mobile navigation toggle with js-button

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,17 @@
-import myFunction, { multiply } from "./myModule";
+// import myFunction, { multiply } from "./myModule";
 
-myFunction();
+// myFunction();
 
-const result = multiply(2, 5);
+// const result = multiply(2, 5);
 
-console.log(result);
+// console.log(result);
+
+console.log("it works!");
+
+const burger_menu = document.querySelector(".burger-menu");
+const overlay = document.querySelector(".overlay");
+
+burger_menu.addEventListener("click", function () {
+  burger_menu.classList.toggle("burger-menu--clicked");
+  overlay.classList.toggle("overlay--visible");
+});
